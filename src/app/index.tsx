2e8fd3e51bf306c06e71aed68c7d8eec5f136357
nichol6s <nicholas.jake15@gmail.com>
2024-02-23 20:00:12 -0300

    Instalando e configurando o tailwindcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import {View, Text} from "react-native"
 
 export default function Home() {
   return (
-    <View>
-        <Text>Hello React Nativers!</Text>
+    <View className=" bg-slate-900 flex-1">
+        <Text className=" text-white">Hello React Nativers!</Text>
     </View>
   )
 }
