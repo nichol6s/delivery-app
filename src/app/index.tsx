7ce399c9ea355995b36230766fdd5cd40b9daf89
nichol6s <nicholas.jake15@gmail.com>
2024-02-23 22:26:32 -0300

    Primeira etapa do aplicativo feita, criando componentes, layout e estrutura de dados

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,31 @@
-import {View, Text} from "react-native"
+import { useState } from "react"
+import { CategoryButton } from "@/components/category-button"
+import { Header } from "@/components/header"
+import { View, FlatList } from "react-native"
+import { CATEGORIES } from "@/utils/data/products"
 
 export default function Home() {
+
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string){
+    setCategory(selectedCategory)
+  }
+
   return (
-    <View className=" bg-slate-900 flex-1">
-        <Text className=" text-white">Hello React Nativers!</Text>
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantitiyItems={1}/>
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item)=> item}
+        renderItem={({item}) => (
+        <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />)}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+      />
     </View>
   )
 }
