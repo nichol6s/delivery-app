a328a7437b0fd3c3a9dba260660f7c2acd477f29
nichol6s <nicholas.jake15@gmail.com>
2024-03-04 19:19:45 -0300

    Instalando a biblioteca "KeyboardAwareScrollView" para facilitar a digitação no input. Design do carrinho finalizado

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,42 +1,74 @@
-import { View, Text, ScrollView } from "react-native"
+import { View, Text, ScrollView, Alert } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
-import { useCartStore } from "@/stores/cart-store"
+import { ProductCartProps, useCartStore } from "@/stores/cart-store"
+
+import { formatCurrency } from "@/utils/functions/format-currency"
 
 import { Header } from "@/components/header"
 import { Input } from "@/components/input"
 import { Product } from "@/components/product"
-import { formatCurrency } from "@/utils/functions/format-currency"
+import { Button } from "@/components/button"
+import { LinkButton } from "@/components/link-button"
 
 export default function Cart() {
     const cartStore = useCartStore()
 
     const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
 
+    function handleProductRemove(product: ProductCartProps){
+        Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [
+            {
+                text: "Cancelar"
+            },
+            {
+                text: "Remover",
+                onPress: () => cartStore.remove(product.id),
+            }
+        ])
+    }
+
     return (
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho" />
 
-            <ScrollView>
-                {cartStore.products.length > 0 ? (
+            <KeyboardAwareScrollView showsVerticalScrollIndicator={false} extraHeight={100}>
+                <View className="p-5 flex-1">
+                    <ScrollView>
+                        {cartStore.products.length > 0 ? (
+                            <View className="border-b border-slate-700">
+                                {cartStore.products.map((product) => (
+                                    <Product key={product.id} data={product} onPress={() => handleProductRemove(product)} />
+                                ))}
+                            </View>
+                        ) : (
+                            <Text className="font-body text-slate-400 text-center my-8">Seu carrinho está vazio.</Text>
+                        )}
 
-                    <View className="p-5 flex-1">
-                        {cartStore.products.map((product) => (
-                            <Product key={product.id} data={product} />
-                        ))}
-                    </View>
-                ) : (
-                    <Text className="font-body text-slate-400 text-center my-8">Seu carrinho está vazio.</Text>
-                )}
+                        <View className="flex-row gap-2 items-center mt-5 mb-4">
+                            <Text className="text-white text-xl font-subtitle">Total: </Text>
 
-                <View className="flex-row gap-2 items-center mt-5 mb-4">
-                    <Text className="text-white text-xl font-subtitle">Total: </Text>
+                            <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+                        </View>
 
-                    <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+                        <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento" />
+
+                    </ScrollView>
                 </View>
+            </KeyboardAwareScrollView>
 
-                <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento" />
+            <View className="p-5 gap-5">
+                <Button>
+                    <Button.Text>Enviar pedido</Button.Text>
+                    <Button.Icon>
+                        <Feather name="arrow-right-circle" size={20} />
+                    </Button.Icon>
+                </Button>
 
-            </ScrollView>
+                <LinkButton title="Voltar ao cardápio" href="/" />
+            </View>
+
         </View>
     )
 } 
